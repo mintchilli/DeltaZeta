7b83ec620167e968fcede1d64b268c5892f78d6e
micm2111 <marcel.michel@usherbrooke.ca>
2019-09-18 22:22:14 -0400

    trying

--- DeltaZetaPhi/Startup.cs
+++ DeltaZetaPhi/Startup.cs
@@ -49,7 +49,7 @@ namespace DeltaZetaPhi
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
